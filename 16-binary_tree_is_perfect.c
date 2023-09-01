46b847f896c409e0e381b84710fd396fc9606389
Omar <omar0642624692@gmail.com>
2023-09-01 05:02:45 -0700

    a function that checks if a binary tree is perfect

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -15,15 +15,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	height = (int)binary_tree_height(tree);
 	leaves = (int)binary_tree_leaves(tree);
 	if (binary_tree_is_full(tree) && !binary_tree_balance(tree))
-	{
-		if (leaves == (1 << height))
-			return (1);
-		else
-			printf("%d\n", 404);
-	}
-	printf("%d\n", leaves);
-	printf("%d\n", height);
-	printf("%d\n", (1 << (height - 1)));
+		return (1 && leaves == (1 << (height - 1)));
 	return (0);
 }
 
